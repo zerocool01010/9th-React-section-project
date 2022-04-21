5668988be030b7e1491c5d058bafbef9e5c4d791
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-04-21 17:53:02 -0300

    Updated al video 107, seccion 9. Manipulando el DOM con refs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,38 +6,49 @@ import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const nameInputRef = useRef() //el ref me conecta con un elemento como el input que recibe el valor de la constante mas abajo en el attr ref
+  const ageInputRef = useRef()
+
+  /* const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState(''); */
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    console.log(nameInputRef); //podremos ver el contenido DOM del input al que hacemos ref
+    console.log(nameInputRef.current.value); //y podemos acceder a los valores del json al que hacemos ref
+
+    const enteredInputName = nameInputRef.current.value
+    const enteredInputAge = ageInputRef.current.value
+
+    if (enteredInputName.trim().length === 0 || enteredInputAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredInputAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
+    props.onAddUser(enteredInputName, enteredInputAge);
+    /* setEnteredUsername('');
+    setEnteredAge(''); */
+    nameInputRef.current.value = '' //usamos esta alternativa que es algo asi como manipular el DOM para resetear el valor de los inputs en lugar de usar los States de arriba que dependen de mas codigo
+    ageInputRef.current.value = '' //pero no siempre es recomendable jugar asi con el DOM, es algo que hay que ver que conviene, si mas codigo y menos manipulacion, o menos codigo y mas manipulacion
   };
 
-  const usernameChangeHandler = (event) => {
+  /* const usernameChangeHandler = (event) => {
     setEnteredUsername(event.target.value);
   };
 
   const ageChangeHandler = (event) => {
     setEnteredAge(event.target.value);
-  };
+  }; */
 
   const errorHandler = () => {
     setError(null);
@@ -58,15 +69,17 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            /* value={enteredUsername}
+            onChange={usernameChangeHandler} */
+            ref={nameInputRef} 
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            /* value={enteredAge}
+            onChange={ageChangeHandler} */
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
